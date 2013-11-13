afc8743079dca6daa90e508697579737bb32cb8e
Anthony Madhvani <anny.mdvn@gmail.com>
2013-11-13 22:24:20 +0100

    Add maps

---
+++ App/App/js/maps.js
@@ -0,0 +1,12 @@
+﻿Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: initMap });
+function initMap() {
+    var map;
+
+    var mapOptions =
+    {
+        credentials: "AtnRnvtS2tavLV6OaHT3DJwmhWvOC0Vyiw4Pponx_vTLUDoOXrwwKjQvJRlZQMUb"
+    };
+
+    map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+    map.setView({ center: new Microsoft.Maps.Location(50.80, 4.4), zoom: 9 });
+}

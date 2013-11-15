eff8af3d291f24bcc1f37baadc810e3044f1389c
Ilias Ismanalijev <hello@illyism.com>
2013-11-15 16:28:00 +0100

    navbar

--- App/Pooplr/js/ui.js
+++ App/Pooplr/js/ui.js
@@ -1,3 +1,16 @@
 ﻿
 
 var uiSettings = new Windows.UI.ViewManagement.UISettings();
+
+
+(function () {
+
+    var navbarInvoked = function (ev) {
+        var navbarCommand = ev.detail.navbarCommand;
+        WinJS.log && WinJS.log(navbarCommand.label + " NavBarCommand invoked", "sample", "status");
+        document.querySelector('select').focus();
+    }
+
+    $('#NavBar').on('invoked', navbarInvoked);
+
+})();
